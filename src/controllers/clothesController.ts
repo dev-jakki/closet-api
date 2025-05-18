91d8ef84a665a706df12796c52264a39275174f8
dev-jakki <jackcielfelix.dev@gmail.com>
2025-05-18 16:46:09 -0300

    fix: teste

--- src/controllers/clothesController.ts
+++ src/controllers/clothesController.ts
@@ -1,4 +1,4 @@
-const { PrismaClient } = require('@prisma/client');
+import { PrismaClient } from "@prisma/client";
 import type { Request, Response } from 'express';
 
 const prisma = new PrismaClient();
